08798875ac725ea499ad24f245c73348eb189fb7
鱼香ROS <87068644+fishros@users.noreply.github.com>
2024-12-07 15:51:12 +0800

    feat: 完成基本功能迁移

---
+++ test/example_main.cpp
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+#include "fishbot.h"
+
+void fishbot_loop_transport_task(void *param)
+{
+  while (true)
+  {
+    loop_fishbot_transport();
+  }
+}
+
+void setup()
+{
+  setup_fishbot();
+  xTaskCreatePinnedToCore(fishbot_loop_transport_task, "fishbot_loop_transport_task", 10240, NULL, 1, NULL, 0);
+}
+
+void loop()
+{
+  delay(10);
+  loop_fishbot_control();
+}
